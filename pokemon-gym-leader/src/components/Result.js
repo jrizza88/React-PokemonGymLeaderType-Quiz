97a5caef90406f94b083b23ca843d49b71a4b552
Jamar Torres <Jetorres88@gmail.com>
2020-08-07 17:16:44 -0400

    created a result component to display results upon completion of the quiz

---
+++ pokemon-gym-leader/src/components/Result.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Result = ({result}) => {
+    return (
+        <div>
+            You are best to be a {result} type of Gym Leader!
+        </div>
+    )
+}
+
+export default Result;
